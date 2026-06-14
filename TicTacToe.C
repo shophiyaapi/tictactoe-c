8bce5eb465024a2076ade19a45ec94b0fcefe5b6
shophiyaa33 <shophiyast@gmail.com>
2026-06-14 13:00:14 +0545

    added missing header file

--- TicTacToe.C
+++ TicTacToe.C
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<windows.h>//to refresh display and clear screen
 
 //board
@@ -118,4 +119,5 @@ int main()
          player++;
     }
     system("pause");
+
 }
